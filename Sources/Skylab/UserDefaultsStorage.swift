b23fb843472765f5239b1e6c43711e11bcb3e5da
Alex Belov <belov.alexandr1991@gmail.com>
2021-05-19 17:11:50 +0800

    avoid race conditions in storage
    Our app has faced 58 crashes since we started using Skylab. I see that the access to the map property isn't synchronised.
    
    For example, DefaultSkylabClient.fetchAll works with map on a background thread. At the same time the client may access map from another thread. It leads to a race condition, which mean lead to a crash

--- Sources/Skylab/UserDefaultsStorage.swift
+++ Sources/Skylab/UserDefaultsStorage.swift
@@ -11,55 +11,76 @@ class UserDefaultsStorage: Storage {
     let userDefaults = UserDefaults.standard
     let sharedPrefsKey: String
     let sharedPrefsPrefix = "com.amplitude.flags.cached."
-    var map: [String:Variant] = [:]
+    private var map: [String:Variant] = [:]
+    private let mapLock = NSLock()
 
     init(apiKey: String) {
         sharedPrefsKey = sharedPrefsPrefix + apiKey
     }
 
     func put(key: String, value: Variant) -> Variant? {
-        let oldValue = self.get(key: key)
-        map[key] = value
-        return oldValue
+        mapLock.withCriticalScope {
+            let oldValue = map[key]
+            map[key] = value
+            return oldValue
+        }
     }
 
     func get(key: String) -> Variant? {
-        return map[key]
+        mapLock.withCriticalScope {
+            return map[key]
+        }
     }
 
     func clear() {
-        map = [:]
+        mapLock.withCriticalScope {
+            map = [:]
+        }
     }
 
     func getAll() -> [String:Variant] {
-        let copy = map
-        return copy
+        mapLock.withCriticalScope {
+            return map
+        }
     }
 
     func load() {
-        if
-            let data = userDefaults.value(forKey: self.sharedPrefsKey) as? Data,
-            let loaded = try? JSONDecoder().decode([String:Variant].self, from: data) {
-            for (key, value) in loaded {
-                map[key] = value
+        mapLock.withCriticalScope {
+            if
+                let data = userDefaults.value(forKey: self.sharedPrefsKey) as? Data,
+                let loaded = try? JSONDecoder().decode([String:Variant].self, from: data) {
+                for (key, value) in loaded {
+                    map[key] = value
+                }
+                return
             }
-            return
-        }
 
-        if
-            let loaded = userDefaults.dictionary(forKey: self.sharedPrefsKey) as? [String:String] {
-            for (key, value) in loaded {
-                map[key] = Variant(value)
+            if
+                let loaded = userDefaults.dictionary(forKey: self.sharedPrefsKey) as? [String:String] {
+                for (key, value) in loaded {
+                    map[key] = Variant(value)
+                }
+                return
             }
-            return
         }
     }
 
     func save() {
-        if let data = try? JSONEncoder().encode(map) {
-            userDefaults.set(data, forKey: self.sharedPrefsKey)
+        mapLock.withCriticalScope {
+            if let data = try? JSONEncoder().encode(map) {
+                userDefaults.set(data, forKey: self.sharedPrefsKey)
+            }
         }
     }
 
 
+}
+
+private extension NSLock {
+    func withCriticalScope<T>(_ block: () -> T) -> T {
+        lock()
+        let value = block()
+        unlock()
+        return value
+    }
 }
